1c0856b93c7e65256d42f24fe885e85de74f5dd2
Parth <parth.adroja@solutionanalysts.com>
2017-07-14 19:34:02 +0530

    Added Page Control

--- PDFKit-iOS/PDFKit-iOS/PDFDocView.swift
+++ PDFKit-iOS/PDFKit-iOS/PDFDocView.swift
@@ -21,6 +21,7 @@ class PDFDocView: UIView {
             pdfScrollView.setPDFPage(page)
         }
     }
+    var pageControlView: UIView!
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -50,8 +51,47 @@ class PDFDocView: UIView {
     }
 
     func loadPDFInView() {
-        pdfScrollView = TiledPDFScrollView(frame: self.bounds)
+        if numberOfPages > 1 {
+            pdfScrollView = TiledPDFScrollView(frame: CGRect(x: 0, y: 50, w: self.bounds.width, h: self.bounds.height - 50))
+            addPageControlView()
+        } else {
+            pdfScrollView = TiledPDFScrollView(frame: self.bounds)
+        }
         self.addSubview(pdfScrollView)
         currentPageNumber = 1
+    }
+
+    func addPageControlView() {
+        pageControlView = UIView(frame: CGRect(x: 0, y: 0, width: self.bounds.width, height: 50))
+        self.addSubview(pageControlView)
+
+
+        func addButtonsToPageControl() {
+            let nextButton = UIButton(x: self.bounds.width - 50, y: 0, w: 50, h: 50, target: self, action: #selector(changePdfPage(buttonTapped:)))
+            nextButton.tag = 2
+            nextButton.setImage(UIImage(named: "img_pdf_next"), for: .normal)
+            pageControlView.addSubview(nextButton)
+
+            let previousButton = UIButton(x: 0, y: 0, w: 50, h: 50, target: self, action: #selector(changePdfPage(buttonTapped:)))
+            previousButton.tag = 1
+            previousButton.setImage(UIImage(named: "img_pdf_previous"), for: .normal)
+            pageControlView.addSubview(previousButton)
+        }
+        addButtonsToPageControl()
+    }
+
+    func changePdfPage(buttonTapped: UIButton) {
+        if buttonTapped.tag == 1 {
+            currentPageNumber = (currentPageNumber - 1).clamp(min: 1, numberOfPages)
+        } else {
+            currentPageNumber = (currentPageNumber + 1).clamp(min: 1, numberOfPages)
+        }
+    }
+
+}
+
+extension Int {
+    func clamp(min: Int, _ max: Int) -> Int {
+        return Swift.max(min, Swift.min(max, self))
     }
 }
